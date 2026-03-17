0fe31bf57d9edecb4eba93e559c75c0a12ab04ca
krpcn1r <zaystep09@gmail.com>
2026-03-17 21:15:45 +0300

    Add file structure

--- kp/main.cpp
+++ kp/main.cpp
@@ -1,9 +1,44 @@
 ﻿#include <iostream>
+#include <conio.h>
+#include <string>
+
+void showAdminMenu() {
+    bool running = true;
+    while (running) {
+        system("cls");
+        std::cout << "=== ПАНЕЛЬ АДМИНИСТРАТОРА ===\n";
+        std::cout << "1. Управление клиентами\n";
+        std::cout << "2. Статистика сети\n";
+        std::cout << "3. Настройки системы\n";
+        std::cout << "0. Выход\n";
+        std::cout << "\nВыберите пункт: ";
+
+        char choice = _getch(); // Ждем нажатия цифры
+
+        switch (choice) {
+        case '1':
+            // Вызов функции из папки clients
+            std::cout << "\nОткрываем список клиентов...";
+            _getch(); // Пауза
+            break;
+        case '2':
+            std::cout << "\nЗагрузка статистики...";
+            _getch();
+            break;
+        case '3':
+            // Настройки
+            break;
+        case '0':
+            running = false; // Выход из цикла меню
+            break;
+        default:
+            // Если нажали что-то другое, ничего не делаем или пишем ошибку
+            break;
+        }
+    }
+}
 
 int main() {
-	setlocale(LC_ALL, "Russian");
-
-	std::cout << "test";
-
-	return 0;
+    setlocale(LC_ALL, "Russian");
+    showAdminMenu();
 }
