0afd6f2dcdd2ab25f547e54a74f2f1289a697615
krpcn1r <zaystep09@gmail.com>
2026-03-17 19:50:33 +0300

    Change file names

--- kp/kp.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include <iostream>
-using namespace std;
-int main()
-{
-    setlocale(LC_ALL, "Russian");
-    cout << "Hello World!\n";
-}
---
+++ kp/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	std::cout << "test";
+
+	return 0;
+}
