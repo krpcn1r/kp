790df3ae086f4785e48fd988b55baaa7f2fa6c6d
krpcn1r <zaystep09@gmail.com>
2026-03-20 19:57:56 +0300

    include headers in main

--- kp/main.cpp
+++ kp/main.cpp
@@ -1,44 +1,20 @@
 ﻿#include <iostream>
-#include <conio.h>
-#include <string>
+#include "admin/AdminPanel.h"
+#include "admin/ConfigWizard.h"
+#include "auth/AuthManager.h"
+#include "auth/ClientStruct.h"
+#include "auth/Login.h"
+#include "auth/Register.h"
+#include "clients/Billing.h"
+#include "clients/ClientManager.h"
+#include "clients/ClientStruct.h"
+#include "core/Database.h"
+#include "core/InputHandler.h"
+#include "core/Render.h"
+#include "core/Utils.h"
 
-void showAdminMenu() {
-    bool running = true;
-    while (running) {
-        system("cls");
-        std::cout << "=== ПАНЕЛЬ АДМИНИСТРАТОРА ===\n";
-        std::cout << "1. Управление клиентами\n";
-        std::cout << "2. Статистика сети\n";
-        std::cout << "3. Настройки системы\n";
-        std::cout << "0. Выход\n";
-        std::cout << "\nВыберите пункт: ";
-
-        char choice = _getch(); // Ждем нажатия цифры
-
-        switch (choice) {
-        case '1':
-            // Вызов функции из папки clients
-            std::cout << "\nОткрываем список клиентов...";
-            _getch(); // Пауза
-            break;
-        case '2':
-            std::cout << "\nЗагрузка статистики...";
-            _getch();
-            break;
-        case '3':
-            // Настройки
-            break;
-        case '0':
-            running = false; // Выход из цикла меню
-            break;
-        default:
-            // Если нажали что-то другое, ничего не делаем или пишем ошибку
-            break;
-        }
-    }
-}
+using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-    showAdminMenu();
+	cout << "Hello\n";
 }
