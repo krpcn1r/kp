84605d2fcd8650147a646030e0a8f4d478b7eecf
krpcn1r <zaystep09@gmail.com>
2026-04-12 16:21:28 +0300

    Move to pc

--- kp/core/Database.h
+++ kp/core/Database.h
@@ -1,6 +1,18 @@
 #pragma once
+#include "../auth/UserStruct.h"
+#include "../clients/ClientStruct.h"
+#include <string>
+#include <vector>
+
 class Database {
 public:
-	void writeFile(string file, struct data);
-	void readFile(string file);
+  static bool saveUsers(const std::vector<User> &users);
+  static std::vector<User> loadUsers();
+
+  static bool saveClients(const std::vector<Client> &clients);
+  static std::vector<Client> loadClients();
+
+private:
+  static const std::string USERS_FILE;
+  static const std::string CLIENTS_FILE;
 };
